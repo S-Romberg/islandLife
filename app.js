7b17ea714bcc2cece0ac0f88e24d31be93576cbc
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2018-04-13 16:24:56 -0600

    end of hackathon day

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const listURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink"
 const smoothieURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=smoothie"
 const searchURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+const iframething = document.getElementById('picture')
 var pTag = document.createElement('p')
 var h2Tag = document.createElement('h1')
 var ulTag = document.createElement('ul')
@@ -173,4 +174,24 @@ function loadAlcohol(){
       }
     })
   })
+}
+function showStrawberries() {
+  iframething.innerHTML= `<iframe src="https://fallingfruit.org/locations/embed?z=13&y=39.73488&x=-104.99831&m=false&t=roadmap&f=105,1844&c=forager,freegan&l=false&locale=en" width=850 height=600 scrolling="no" style="border:none;"></iframe>
+`
+}
+function showApples() {
+  iframething.innerHTML= `<iframe src="https://fallingfruit.org/locations/embed?z=13&y=39.73488&x=-104.99831&m=false&t=roadmap&f=14,1566,1874,3177,3180,3302,3306,3307,3308,3309,3310,3311,3315,3318,3319,3320,3321,3322,3323,3326,3329,3330,3332,3333,3334,3335,3336,3337,3338,3631,3635,3709,4066&c=forager,freegan&l=false&locale=en" width=640 height=600 scrolling="no" style="border:none;"></iframe>`
+}
+function showPeaches() {
+  iframething.innerHTML= `<iframe src="https://fallingfruit.org/locations/embed?z=13&y=39.73488&x=-104.99831&m=false&t=roadmap&f=3280,3328,52&c=forager,freegan&l=false&locale=en" width=640 height=600 scrolling="no" style="border:none;"></iframe>
+`
+}
+function showBlackberries() {
+  iframething.innerHTML= `<iframe src="https://fallingfruit.org/locations/embed?z=13&y=39.74428&x=-104.96938&m=false&t=roadmap&f=122,123,1426,1471,1741,1886,196,1986,230,2422,2523,2711,3901,4007,4020,420,48,566,701,857,98&c=forager,freegan&l=false&locale=en" width=640 height=600 scrolling="no" style="border:none;"></iframe>
+`
+}
+function showBanana() {
+  iframething.innerHTML= `<iframe src="https://fallingfruit.org/locations/embed?z=13&y=39.73488&x=-104.99831&m=false&t=roadmap&f=1982,75&c=forager,freegan&l=false&locale=en" width=640 height=600 scrolling="no" style="border:none;"></iframe>
+
+`
 }
